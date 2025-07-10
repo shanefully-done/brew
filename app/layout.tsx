6ee670047c4ca6b62f5efd8acac4f295aed4d7de
Shane Lx <39976362+shanefully-done@users.noreply.github.com>
2025-07-10 23:08:31 +0900

    Added favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,27 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en" suppressHydrationWarning>
+			<head>
+				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="192x192"
+					href="/web-app-manifest-192x192.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="512x512"
+					href="/web-app-manifest-512x512.png"
+				/>
+				<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+				<link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+				<link rel="shortcut icon" href="/favicon.ico" />
+				<link rel="manifest" href="/site.webmanifest" />
+			</head>
 			<body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
 				<ThemeProvider
 					attribute="class"
